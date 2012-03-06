342f65c691317d8f0ecf9a27b80d671db2dccfad
luke <kejinlu@gmail.com>
2012-03-06 11:37:27 +0800

    Initial Commit

---
+++ UIInsetsLabel/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UIInsetsLabel
+//
+//  Created by Lu Kejin on 3/6/12.
+//  Copyright (c) 2012 Taobao.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ UIInsetsLabel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIInsetsLabel
+//
+//  Created by Lu Kejin on 3/6/12.
+//  Copyright (c) 2012 Taobao.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
